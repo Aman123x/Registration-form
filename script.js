73a2d584024a973672c8bc8c4d8b3663a00e817f
Aman <aman01428@gmail.com>
2023-11-06 01:43:24 +0530

    06Nov

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 
-const date=new Date();
 
-document.getElementById('myForm').addEventListener('submit',function(e){
-    e.preventDefault();
-    console.log("Name: "+ document.getElementById('name').value);
-    console.log("Email: "+ document.getElementById('email').value);
-    console.log("Phone: "+ document.getElementById('Phone').value);
-    console.log("Date: "+ document.getElementById('date').value);
-    console.log("Time: "+ document.getElementById('time').value);
-})
-  
+
+// document.getElementById('myForm').addEventListener('submit',function(e){
+    //     e.preventDefault();
+    //     console.log("Name: "+ document.getElementById('name').value);
+    //     console.log("Email: "+ document.getElementById('email').value);
+    //     console.log("Phone: "+ document.getElementById('Phone').value);
+    //     console.log("Date: "+ document.getElementById('date').value);
+    //     console.log("Time: "+ document.getElementById('time').value);
+    // })
+    
+    // Local Storage in JavaScript -->
+    
+    // localStorage.setItem("key","value");
+    // localStorage.getItem("key");
+    // localStorage.removeItem("name");
+const date=new Date();
+let form=document.getElementById("myForm");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let text=document.getElementById("Phone").value;
+    let date=document.getElementById("date").value;
+    let time=document.getElementById("time").value;
+
+    let formData={
+        name:name,
+        email:email,
+        text:text,
+        date:date,
+        time:time
+    };
+
+    let formDataJSON= JSON.stringify(formData);
+
+    localStorage.setItem("formData",formDataJSON);
+
+    console.log(localStorage.getItem("formData"));
+});
