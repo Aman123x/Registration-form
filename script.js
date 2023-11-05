6da1bd17db86aa2706d385b42a80c8d172315a5e
Aman <aman01428@gmail.com>
2023-11-06 01:53:09 +0530

    06Nov2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
 
-// document.getElementById('myForm').addEventListener('submit',function(e){
-    //     e.preventDefault();
-    //     console.log("Name: "+ document.getElementById('name').value);
-    //     console.log("Email: "+ document.getElementById('email').value);
-    //     console.log("Phone: "+ document.getElementById('Phone').value);
-    //     console.log("Date: "+ document.getElementById('date').value);
-    //     console.log("Time: "+ document.getElementById('time').value);
-    // })
+document.getElementById('myForm').addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log("Name: "+ document.getElementById('name').value);
+    console.log("Email: "+ document.getElementById('email').value);
+    console.log("Phone: "+ document.getElementById('Phone').value);
+    console.log("Date: "+ document.getElementById('date').value);
+    console.log("Time: "+ document.getElementById('time').value);
+});
     
     // Local Storage in JavaScript -->
     
