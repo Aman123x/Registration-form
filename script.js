b307aa25164ff6e65c160f98a74240f4dbd61f6d
Aman <aman01428@gmail.com>
2023-11-06 03:49:20 +0530

    06Nov4

--- script.js
+++ script.js
@@ -28,9 +28,78 @@ document.addEventListener("DOMContentLoaded", function() {
                 const storedDataJSON= localStorage.getItem(key);
                 if(storedDataJSON){
                     const storedData=JSON.parse(storedDataJSON);
-                    const userDataDiv=document.createElement("li");
-                    userDataDiv.innerHTML=`Name:${storedData.name} Email: ${storedData.email}  Phone: ${storedData.phone} Date: ${storedData.date} Time: ${storedData.time}<br>`;
-                    displayElement.appendChild(userDataDiv);
+
+                    // Create list item to display stored data
+                    const listItem = document.createElement("li");
+
+                    // Create a container for the "Delete" button and data elements
+                    const container = document.createElement("div");
+
+                    // Create a clickable "Delete" button
+                    const deleteLink = document.createElement("a");
+                    deleteLink.href = "#";
+                    deleteLink.textContent = "Delete";
+                    deleteLink.style.color = "red";
+
+                    // Add a click event listener to the "Delete" link
+                    deleteLink.addEventListener("click", function(e) {
+                        e.preventDefault();
+
+                        // Remove the data from local storage
+                        localStorage.removeItem(key);
+
+                        // Remove the list item from the display
+                        listItem.remove();
+                    });
+
+                    // Append the "Delete" link to the container
+                    container.appendChild(deleteLink);
+
+                    // Create and append data elements to the container
+                    const dataElements = document.createElement("div");
+                    
+                    dataElements.innerHTML = `Name: ${storedData.name}, Email: ${storedData.email}, Phone: ${storedData.phone}, Date: ${storedData.date}, Time: ${storedData.time}`;
+
+                    // Append the data elements to the container
+                    container.appendChild(dataElements);
+
+                    // Append the container to the list item
+                    listItem.appendChild(container);
+
+                    // Append the list item to the display area
+                    displayElement.appendChild(listItem);
+                    // // const userDataDiv=document.createElement("li");
+                    
+                    // // const deleteButton = document.createElement("input");
+                    // // deleteButton.type = "button";
+                    // // deleteButton.value = "Delete";
+                    // // deleteButton.style.backgroundColor="red";
+
+                    // // Create a container for data and the "Delete" button
+                    // const dataContainer = document.createElement("div");
+
+                    // // Create a "Delete" button
+                    // const deleteButton = document.createElement("input");
+                    // deleteButton.type = "button";
+                    // deleteButton.value = "Delete";
+                    // deleteButton.style.backgroundColor = "red";
+
+                    //  // Append the "Delete" button to the data container
+                    //  dataContainer.appendChild(deleteButton);
+
+                    //  // Create and append data elements to the data container
+                    //  const dataElements = document.createElement("div");
+
+                    //  // Append the data elements to the data container
+                    // dataContainer.appendChild(dataElements);
+
+                    // // Append the data container to the list item
+                    // userDataDiv.appendChild(dataContainer);
+
+                    // displayElement.appendChild(userDataDiv);
+
+                    // userDataDiv.innerHTML=`Name:${storedData.name} Email: ${storedData.email}  Phone: ${storedData.phone} Date: ${storedData.date} Time: ${storedData.time}`;
+                    // displayElement.appendChild(userDataDiv);
                 }
             }
         }
