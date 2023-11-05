d38eebb68331c21cf9f5a11d66280cfab16c32e2
Aman <aman01428@gmail.com>
2023-11-06 03:03:01 +0530

    06Nov3

--- script.js
+++ script.js
@@ -15,28 +15,58 @@ document.getElementById('myForm').addEventListener('submit',function(e){
     // localStorage.setItem("key","value");
     // localStorage.getItem("key");
     // localStorage.removeItem("name");
-const date=new Date();
-let form=document.getElementById("myForm");
+//=====================================================
 
-form.addEventListener("submit",function(event){
-    event.preventDefault();
-    let name=document.getElementById("name").value;
-    let email=document.getElementById("email").value;
-    let text=document.getElementById("Phone").value;
-    let date=document.getElementById("date").value;
-    let time=document.getElementById("time").value;
-
-    let formData={
-        name:name,
-        email:email,
-        text:text,
-        date:date,
-        time:time
-    };
-
-    let formDataJSON= JSON.stringify(formData);
-
-    localStorage.setItem("formData",formDataJSON);
-
-    console.log(localStorage.getItem("formData"));
+document.addEventListener("DOMContentLoaded", function() {
+    function displayStoredData(){
+        const displayElement=document.getElementById("displayData");
+        //displayElement.innerHTML="";
+    
+        for(let i=0;i<localStorage.length;i++){
+            const key=localStorage.key(i);
+            if(key.startsWith("formData_")){
+                const storedDataJSON= localStorage.getItem(key);
+                if(storedDataJSON){
+                    const storedData=JSON.parse(storedDataJSON);
+                    const userDataDiv=document.createElement("li");
+                    userDataDiv.innerHTML=`Name:${storedData.name} Email: ${storedData.email}  Phone: ${storedData.phone} Date: ${storedData.date} Time: ${storedData.time}<br>`;
+                    displayElement.appendChild(userDataDiv);
+                }
+            }
+        }
+    }
+    
+    //========================================================    
+    const date=new Date();
+    let form=document.getElementById("myForm");
+    
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        let name=document.getElementById("name").value;
+        let email=document.getElementById("email").value;
+        let phone=document.getElementById("Phone").value;
+        let date=document.getElementById("date").value;
+        let time=document.getElementById("time").value;
+    
+        let formData={
+            name:name,
+            email:email,
+            phone:phone,
+            date:date,
+            time:time
+        };
+    
+        let formDataJSON= JSON.stringify(formData);
+        let unquieKey=Date.now().toString();
+    
+        localStorage.setItem(`formData_${unquieKey}`,formDataJSON);
+    
+        console.log(localStorage.getItem("formData"));
+        // displayStoredData();
+    });
+    
+    displayStoredData();
 });
+
+
+
